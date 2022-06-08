2d1d057366237fab33d0c7759a76d96153a37741
Kevin <kevinfiorentino.1294@gmail.com>
2022-06-08 15:04:12 -0300

    Endpoints GET with Params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('new-endpoint')
+  newEndpoint(): string {
+    return 'New endpoint';
+  }
+
+  @Get('product/:idProduct')
+  getProduct1(@Param() params: any): string {
+    return `Producto id: ${params.idProduct}`;
+  }
+
+  @Get('product/:idProduct')
+  getProduct2(@Param('idProduct') idProduct: string): string {
+    return `Producto id: ${idProduct}`;
+  }
+
 }
